fd1fdeba7d4eb6c8b1575a8c1947d6c05281028b
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-06-29 20:06:20 +0300

    DO WHILE. Task 1

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * DO WHILE. Task 1
+ * @type {number}
+ */
+let userInput = Number(prompt('Введите ваше значение'));
+let count = 0;
+
+while (count < userInput) {
+    ++count;
+    alert('#');
+
+};
