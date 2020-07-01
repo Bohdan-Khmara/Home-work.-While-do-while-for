38deab0e2278d98c0731ab2957adfafc8a70d557
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-01 19:29:45 +0300

    DO WHILE. Task 5

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 /**
- * DO WHILE. Task 4
+ *
  * @type {number}
  */
-let firstNumber = Number(prompt('Введите ваше первое число'));
-let secondNumber = Number(prompt('Введите ваше второе число'));
-let count = 1;
-let divider1 = 0;
-let divider2 = 0;
-if (firstNumber > secondNumber) {
-    while (count < firstNumber) {
-        if (firstNumber % count === 0 && secondNumber % count === 0) {
-            divider1 = firstNumber / count;
-            divider2 = secondNumber / count;
-            console.log(divider1, divider2);
-            ++count;
-        } else  {
-            ++count;
-        }
-    }
-} else {
-    while (count < secondNumber) {
-        if (firstNumber % count === 0 && secondNumber % count === 0) {
-            divider1 = firstNumber / count;
-            divider2 = secondNumber / count;
-            console.log(divider1, divider2);
-            ++count;
-        }else {
-            ++count;
-        }
+let value = Number(prompt('Введите ваше число'));
+let factorial = value;
+let count = value;
 
-    }
+while (count > 1) {
+    count--
+    factorial = factorial * count;
+    alert(factorial);
 }
+
 
